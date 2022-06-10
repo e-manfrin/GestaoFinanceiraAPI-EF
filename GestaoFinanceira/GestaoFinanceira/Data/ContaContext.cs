9b324dfda33971a795f7d684f5d14116e20188d8
Emily keyt Manfrin <rm87198@fiap.com.br>
2022-06-10 11:36:50 -0300

    Projeto

---
+++ GestaoFinanceira/GestaoFinanceira/Data/ContaContext.cs
@@ -0,0 +1,15 @@
+﻿using GestaoFinanceira.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestaoFinanceira.Data
+{
+    public class ContaContext : DbContext
+    {
+        public ContaContext(DbContextOptions<ContaContext> opt) : base(opt)
+        {
+
+        }
+        public DbSet<Conta> Contas { get; set; }
+    }
+}
+
