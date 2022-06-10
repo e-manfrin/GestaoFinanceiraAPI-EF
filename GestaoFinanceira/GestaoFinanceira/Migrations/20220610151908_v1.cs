4e599c2de6eff9e42d42260f17344d67bac797c6
Emily keyt Manfrin <rm87198@fiap.com.br>
2022-06-10 12:24:39 -0300

    Projeto rodando com banco de dados SQLITE

---
+++ GestaoFinanceira/GestaoFinanceira/Migrations/20220610151908_v1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestaoFinanceira.Migrations
+{
+    public partial class v1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DataHora = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Valor = table.Column<double>(type: "REAL", nullable: false),
+                    Descricao = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contas");
+        }
+    }
+}
